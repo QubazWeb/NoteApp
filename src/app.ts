b69ecfc1d54eff5c2368c18c6036d5761a0a5e77
QubazWeb <qubazdev@gmail.com>
2025-11-08 13:23:45 +0100

    test commit

--- src/app.ts
+++ src/app.ts
@@ -140,7 +140,7 @@ app.post("/signup", async (req:any, res:any) => {
   }
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
